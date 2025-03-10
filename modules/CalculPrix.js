1d35871b8221e5e9bc276300fa2ada10a0af99ad
sidibe ibrahim <dramansidibe@gmail.com>
2025-03-10 12:28:46 +0100

    fix douane

--- modules/CalculPrix.js
+++ modules/CalculPrix.js
@@ -26,6 +26,8 @@ export function calculProductPrices(
     }
   });
 
+  console.log('data', data)
+
   data.forEach(function (item) {
     prix = parseFloat(item.Price);
     prix = isNaN(prix) ? 0 : prix;
@@ -46,7 +48,7 @@ export function calculProductPrices(
     douane = validationManuelle
       ? null
       : item.product.productSpecificites[0].douane;
-
+      
     if (douane) {
       let forfaitDouane =
         "New" == item.stateValue
@@ -57,15 +59,21 @@ export function calculProductPrices(
           ? douane.coefficient
           : douane.coefficientProduitOccasion;
 
-      if (forfaitDouane) {
-        frais = parseFloat(forfaitDouane);
-        frais = isNaN(frais) ? 0 : frais;
-        frais = frais * quantite;
-      } else if (coefficientDouane) {
-        frais = parseFloat(coefficientDouane);
-        frais = isNaN(frais) ? 0 : frais;
-        frais = (frais * item.productValue) / 100;
+      let minimumDouane = "New" == item.stateValue ? douane.minimumDouaneNeuf : douane.minimumDouaneUsage;
+      
+      if (minimumDouane && item.productValue >= minimumDouane){
+        if (forfaitDouane) {
+          frais = parseFloat(forfaitDouane);
+          frais = isNaN(frais) ? 0 : frais;
+          frais = frais * quantite;
+        } else if (coefficientDouane) {
+          frais = parseFloat(coefficientDouane);
+          frais = isNaN(frais) ? 0 : frais;
+          frais = (frais * item.productValue) / 100;
+        }
       }
+
+      
     }
 
     sommeFraisDouane = sommeFraisDouane + frais;
